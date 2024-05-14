49a86cb2edbd1a117100a12aac611113c395f2b9
Hafiz <HafizullahRahimi@outlooo.com>
2024-05-14 14:18:49 +0200

    Update Images, LatestNews and MainSlider components

--- News24.Web/wwwroot/site/js/custom.js
+++ News24.Web/wwwroot/site/js/custom.js
@@ -1,4 +1,30 @@
-﻿window.ShowToastr = (type, message) => {
+﻿function OnDocumentInit() {
+    jQuery('[data-toggle="tooltip"]').tooltip();
+
+    jQuery('.main-slider').owlCarousel({
+        rtl: true,
+        loop: true,
+        margin: 8,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+}
+
+
+window.ShowToastr = (type, message) => {
     if (type === 'success') {
         toastr.success(message, 'عملیات مورد نظر با موفقیت انجام شد');
     }
